3d4541cb7cce531efbaedb63d1322ca2ad5994a5
Ricardo <ricardonascimento1506@gmail.com>
2026-04-27 15:59:03 -0300

    feat: config no prisma finalizado

--- src/mudules/users/users.service.ts
+++ src/mudules/users/users.service.ts
@@ -1,41 +1,39 @@
 import { Injectable } from '@nestjs/common'
-import { UsersRequestDTO } from './users.dto';
-import { PrismaService } from '../../prisma.service';
+import { PrismaService } from '../../prisma/prisma.service'
+import { UsersRequestDTO } from './users.dto'
 
 @Injectable()
 export class UsersService {
-
-  constructor( private readonly prisma: PrismaService){}
+  constructor(private readonly prisma: PrismaService) {}
 
   findAll() {
     return this.prisma.user.findMany()
   }
   findById(id: string) {
-
     return this.prisma.user.findFirst({
       where: {
-        id
-      }
+        id,
+      },
     })
   }
   create(data: UsersRequestDTO) {
     return this.prisma.user.create({
-      data
+      data,
     })
   }
   update(id: string, data: UsersRequestDTO) {
     return this.prisma.user.update({
       where: {
-        id
+        id,
       },
-      data
+      data,
     })
   }
   remove(id: string) {
     this.prisma.user.delete({
       where: {
-        id
-      }
+        id,
+      },
     })
   }
 }
