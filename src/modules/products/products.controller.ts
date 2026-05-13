1da9629338fb136aae43ae80e1abb0f1f2bf4757
Gabriel-lopes06 <gabriellopes0627@gmail.com>
2026-05-13 14:51:08 -0300

    adjust products.controller and products.service

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
 
-@Controller('products')
-export class ProductsController {}
+
+//precisa passar pela limpa de Ricardo
+
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiResponse,
+} from '@nestjs/swagger'
+
+import {
+  ProductsListItemDTO,
+  ProductsRequestDTO,
+  UpdateProductsDTO,
+} from './products.dto'
+
+import { ProductsService } from './products.service'
+
+@Controller({
+  path: 'products',
+  version: '1',
+})
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  @ApiResponse({
+    type: [ProductsListItemDTO],
+  })
+  findAll() {
+    return this.productsService.findAll()
+  }
+
+  @Get(':productId')
+  @ApiResponse({
+    type: ProductsListItemDTO,
+  })
+  findById(
+    @Param('productId', ParseUUIDPipe)
+    productId: string,
+  ) {
+    return this.productsService.findById(productId)
+  }
+
+  @Post()
+  @ApiCreatedResponse({
+    type: ProductsListItemDTO,
+  })
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() data: ProductsRequestDTO) {
+    return this.productsService.create(data)
+  }
+
+  @Put(':productId')
+  @ApiOkResponse({
+    type: ProductsListItemDTO,
+  })
+  update(
+    @Param('productId', ParseUUIDPipe)
+    productId: string,
+    @Body() data: UpdateProductsDTO,
+  ) {
+    return this.productsService.update(productId, data)
+  }
+
+  @Delete(':productId')
+  @ApiNoContentResponse()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(
+    @Param('productId', ParseUUIDPipe)
+    productId: string,
+  ) {
+    return this.productsService.remove(productId)
+  }
+}
