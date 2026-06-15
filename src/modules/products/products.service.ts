9bc5e36c5737477e0c38eb9b0da6953ca536bc0d
Ricardo <ricardonascimento1506@gmail.com>
2026-06-15 20:48:34 -0300

    feat(products): add support for multiple product images

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -16,13 +16,20 @@ export class ProductsService {
   ) {}
 
   findAll() {
-    return this.prisma.product.findMany()
+    return this.prisma.product.findMany({
+      include: {
+        images: true,
+      },
+    })
   }
 
   findById(id: string) {
     return this.prisma.product.findFirst({
       where: {
         id,
+      },
+      include: {
+        images: true,
       },
     })
   }
@@ -53,7 +60,18 @@ export class ProductsService {
         userId: userId,
         storeId: storeId,
         categoryId: data.categoryId ?? null,
-      }as Prisma.ProductUncheckedCreateInput,
+
+        images: data.images
+          ? {
+              create: data.images.map((url) => ({
+                imageUrl: url,
+              })),
+            }
+          : undefined,
+      } as Prisma.ProductUncheckedCreateInput,
+      include: {
+        images: true,
+      },
     })
   }
 
@@ -62,7 +80,21 @@ export class ProductsService {
       where: {
         id,
       },
-      data,
+      data: {
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock,
+
+        ...(data.categoryId && {
+          category: { 
+            connect: { id: data.categoryId },
+          },
+        }),
+      },
+      include:{
+        images: true
+      }
     })
   }
 
